448b7eced002558dc337c837b57cab3182c1231c
hiro266 <t1freejob@gmail.com>
2020-04-06 20:57:13 +0900

    loginメソッドをrails_helper.rbで読み込ませることでsystem/以下のファイルで使用可能に

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# login_module.rbを読み込み
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -61,4 +65,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  # module読み込み
+  config.include LoginModule
 end
